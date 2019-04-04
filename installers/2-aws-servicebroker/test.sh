151c6330ae18add2fe57268d48d6451e77582243
Brendan Gordon <brendan.gordon@digital.gov.au>
2019-04-04 15:56:28 +1100

    Add starter aws-sb test

---
+++ installers/2-aws-servicebroker/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "Test aws-servicebroker"
+
+svcat describe broker aws-servicebroker
+
+svcat sync broker aws-servicebroker
+
+echo "Wait for at least one entry in the marketplace"
+end=$((SECONDS+5))
+while :
+do
+  if (( "$(svcat marketplace -o json | jq '. | length')" > "0" )); then
+    break;
+  fi
+  if (( ${SECONDS} >= end )); then
+    echo "Timeout: Wait for at least one entry in the marketplace"
+    exit 1
+  fi
+  sleep 5
+done
